0a994bbd1cb23085eb115ffbfff615d21db41ecb
Lutsko Dmitriy <dmitriy.lutsko@ilogos-ua.com>
2013-04-07 09:59:14 +0300

    before bootstrap

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,3 @@
+class PostsController < CrudController
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :pass
   has_many :posts
+
+  def to_s
+    "#{name}"
+  end
 end
